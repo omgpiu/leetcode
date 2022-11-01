c8d36bd382538cebc2593fe75803e397637f7d87
omgpiu <dyba@list.ru>
2022-11-01 09:32:43 +0300

    search

--- leetcode.js
+++ leetcode.js
@@ -804,3 +804,28 @@ var peakIndexInMountainArray = function(arr) {
 
     return left+1
 };
+
+var search = function(nums, target) {
+    let left=0
+    let right = nums.length -1
+
+    while(left <= right){
+        let middle = Math.floor((left+right)/2)
+        let result = nums[middle]
+        if(result===target){
+            return middle
+        }
+
+        if(result > target){
+            right = middle-1
+        }else {
+            left = middle+1
+        }
+
+
+
+    }
+    return -1
+
+
+};
